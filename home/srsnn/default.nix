891dd3095dfd8c8cb42a8864ab6d267d3f449918
srsng <srsnng@hotmail.com>
2026-05-25 00:13:48 +0800

    chore 移除用户nix中多余的配置

--- home/srsnn/default.nix
+++ home/srsnn/default.nix
@@ -20,35 +20,9 @@
     ../.config/hypr.nix # hyprland
   ];
 
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-    # ".config/hypr" = {
-    #   source = config.lib.file.mkOutOfStoreSymlink ./.config/hypr;
-    #   # source = ./.config/hypr;
-    #   recursive = true;
-    # };
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
   home.sessionVariables = {
     EDITOR = "code";
   };
-
-  # # programs.foot = {
-  # #   enable = true;
-  # #   font = {
-  # #     name = "Sarasa Mono SC";
-  # #     size = 12;
-  # #   };
-  # # };
 
   programs.home-manager.enable = true;
 }
