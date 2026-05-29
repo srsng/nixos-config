8ea918ba53943533b58aec8780299756e4134adb
srsng <srsnng@hotmail.com>
2026-05-30 04:15:25 +0800

    chore 添加一个caelestia的fastfetch配置

--- home/.config/fastfetch.nix
+++ home/.config/fastfetch.nix
@@ -7,5 +7,6 @@
   home.packages = with pkgs; [ fastfetch ];
 
   # TODO: or Frost-Phoenix-nixos-config-fastftech.jsonc
-  xdg.configFile."fastfetch/config.jsonc".source = ./fastfetch/Horizon0427-Arch-Config-fastfetch.jsonc;  
+  # Horizon0427-Arch-Config-fastfetch.jsonc
+  xdg.configFile."fastfetch/config.jsonc".source = ./fastfetch/caelestia.jsonc;  
 }
