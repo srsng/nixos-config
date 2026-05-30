00b6095fdb0d37cfc517a0d6d8d604919b3ee660
srsng <srsnng@hotmail.com>
2026-05-30 17:49:44 +0800

    feat init config for seven-nix

---
+++ hosts/seven-nix/configuration.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/host-binary-cache.nix
+    ../../modules/base.nix
+    ../../modules/desktop-hyprland.nix
+    ../../modules/dev.nix
+    ../../modules/ssh.nix
+  ];
+
+  networking.hostName = "nixos";
+
+  # host-binary-cache.nix
+  srsnn.hostBinaryCache.enable = true;
+
+  # Required services
+  services.geoclue2.enable = true; # For QtPositioning
+  # services.networkmanager.enable = true; # For network management
+
+  # Keep this at the release version used for the first install.
+  # Do not change it just because you upgrade nixpkgs.
+  system.stateVersion = "25.11";
+}
